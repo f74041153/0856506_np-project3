48e27787a336a0e1cce5413499383abd3d5b1282
rslai <rslai@nplinux12.cs.nctu.edu.tw>
2019-12-16 11:29:20 +0800

    formau

--- console.cpp
+++ console.cpp
@@ -11,7 +11,8 @@
 using namespace std;
 using namespace boost::asio;
 
-struct RemoteServer{
+struct RemoteServer
+{
 	string host;
 	string port;
 	string file;
@@ -20,218 +21,268 @@ struct RemoteServer{
 
 io_service global_io_service;
 
-class ConsoleSession : public enable_shared_from_this<ConsoleSession> {
- private:
-  enum { max_length = 4096 };
-  ip::tcp::resolver::query q;
-  ip::tcp::resolver resolv{global_io_service};
-  ip::tcp::socket tcp_socket{global_io_service};
-  array<char, max_length> _data;
-  struct RemoteServer RS;
-  ifstream testcase;
+class ConsoleSession : public enable_shared_from_this<ConsoleSession>
+{
+private:
+	enum
+	{
+		max_length = 4096
+	};
+	ip::tcp::resolver::query q;
+	ip::tcp::resolver resolv{global_io_service};
+	ip::tcp::socket tcp_socket{global_io_service};
+	array<char, max_length> _data;
+	struct RemoteServer RS;
+	ifstream testcase;
 
- public:
-  ConsoleSession(struct RemoteServer rs) : q(rs.host,rs.port), RS(rs),testcase("test_case/"+rs.file){}
+public:
+	ConsoleSession(struct RemoteServer rs) : q(rs.host, rs.port), RS(rs), testcase("test_case/" + rs.file) {}
 
-  void start() { 
- 	do_resolve();	
-  }
-
- private:
-  void do_resolve(){
-  	auto self(shared_from_this());
-	resolv.async_resolve(q , [this,self](boost::system::error_code ec,ip::tcp::resolver::iterator it){
-		if(!ec) do_connect(it);			
-	});
-		
-  }
-  void do_connect(ip::tcp::resolver::iterator it){
-  	auto self(shared_from_this());
-	tcp_socket.async_connect(*it, [this,self](boost::system::error_code ec){
-		if(!ec) do_read();
-	});
-  }
-  void do_read() {
-    auto self(shared_from_this());
-    tcp_socket.async_read_some(
-        buffer(_data, max_length),
-        [this, self](boost::system::error_code ec, size_t length) {
-          	if (!ec){
-	  		string shell2client(_data.data(),length);
-			output_shell(RS.id,shell2client);
-			if(percent_exist(shell2client)){
-				string cmd;
-				getline(testcase,cmd);
-				cmd += "\n";
-				output_command(RS.id,cmd);;
-				do_write(cmd);
-			}else{
-				do_read();
-			}
-		}else{
-			testcase.close();
-		}
-        });
-  }
-
-  void do_write(string to_rs) {
-    auto self(shared_from_this());
-    tcp_socket.async_send(
-        buffer(to_rs),
-        [this, self](boost::system::error_code ec, size_t /* length */) {
-          if (!ec) do_read();
-        });
-  }
-
-  bool percent_exist(string str){
-  	for(unsigned int i=0;i<str.size();i++){
-		if(str[i]=='%')
-			return true;
+	void start()
+	{
+		do_resolve();
 	}
-	return false;
-  }
- 
-  void html_escape(string& data){
-	  string buffer;
-	  buffer.reserve(data.size());
-	  for(size_t pos=0;pos!=data.size();++pos){
-	  	switch(data[pos]){
-			case '&' : buffer.append("&amp;");	break;
-			case '\"': buffer.append("&quot;");	break;
-			case '\'': buffer.append("&apos;");	break;
-			case '<' : buffer.append("&lt;");	break;
-	   		case '>' : buffer.append("&gt;");	break;
-			case '\n': buffer.append("&NewLine;");	break;
-			case '\r': buffer.append("");		break;
-			default  : buffer.append(&data[pos],1);	break;			   
+
+private:
+	void do_resolve()
+	{
+		auto self(shared_from_this());
+		resolv.async_resolve(q, [this, self](boost::system::error_code ec, ip::tcp::resolver::iterator it) {
+			if (!ec)
+				do_connect(it);
+		});
+	}
+	void do_connect(ip::tcp::resolver::iterator it)
+	{
+		auto self(shared_from_this());
+		tcp_socket.async_connect(*it, [this, self](boost::system::error_code ec) {
+			if (!ec)
+				do_read();
+		});
+	}
+	void do_read()
+	{
+		auto self(shared_from_this());
+		tcp_socket.async_read_some(
+			buffer(_data, max_length),
+			[this, self](boost::system::error_code ec, size_t length) {
+				if (!ec)
+				{
+					string shell2client(_data.data(), length);
+					output_shell(RS.id, shell2client);
+					if (percent_exist(shell2client))
+					{
+						string cmd;
+						getline(testcase, cmd);
+						cmd += "\n";
+						output_command(RS.id, cmd);
+						do_write(cmd);
+					}
+					else
+					{
+						do_read();
+					}
+				}
+				else
+				{
+					testcase.close();
+				}
+			});
+	}
+
+	void do_write(string to_rs)
+	{
+		auto self(shared_from_this());
+		tcp_socket.async_send(
+			buffer(to_rs),
+			[this, self](boost::system::error_code ec, size_t /* length */) {
+				if (!ec)
+					do_read();
+			});
+	}
+
+	bool percent_exist(string str)
+	{
+		for (unsigned int i = 0; i < str.size(); i++)
+		{
+			if (str[i] == '%')
+				return true;
 		}
-	  }
-	  data.swap(buffer);
-  } 
+		return false;
+	}
 
-  void output_shell(int rsid,string content){
-	// client <- console <- RS
-  	html_escape(content);
-	string rsp = "<script>document.getElementById('s"+ to_string(rsid) +"').innerHTML += \'"+ content +"\';</script>";
-	write(1,rsp.c_str(),rsp.size()); 
-  }
+	void html_escape(string &data)
+	{
+		string buffer;
+		buffer.reserve(data.size());
+		for (size_t pos = 0; pos != data.size(); ++pos)
+		{
+			switch (data[pos])
+			{
+			case '&':
+				buffer.append("&amp;");
+				break;
+			case '\"':
+				buffer.append("&quot;");
+				break;
+			case '\'':
+				buffer.append("&apos;");
+				break;
+			case '<':
+				buffer.append("&lt;");
+				break;
+			case '>':
+				buffer.append("&gt;");
+				break;
+			case '\n':
+				buffer.append("&NewLine;");
+				break;
+			case '\r':
+				buffer.append("");
+				break;
+			default:
+				buffer.append(&data[pos], 1);
+				break;
+			}
+		}
+		data.swap(buffer);
+	}
 
-  void output_command(int rsid,string content){
-  	// client <- console -> RS
-	html_escape(content);
-	string rsp = "<script>document.getElementById('s"+ to_string(rsid) +"').innerHTML += \'"+ content +"\';</script>";
-	write(1,rsp.c_str(),rsp.size());
-  }
+	void output_shell(int rsid, string content)
+	{
+		// client <- console <- RS
+		html_escape(content);
+		string rsp = "<script>document.getElementById('s" + to_string(rsid) + "').innerHTML += \'" + content + "\';</script>";
+		write(1, rsp.c_str(), rsp.size());
+	}
 
+	void output_command(int rsid, string content)
+	{
+		// client <- console -> RS
+		html_escape(content);
+		string rsp = "<script>document.getElementById('s" + to_string(rsid) + "').innerHTML += \'" + content + "\';</script>";
+		write(1, rsp.c_str(), rsp.size());
+	}
 };
 
-void set_remote_server(struct RemoteServer rs[]){
+void set_remote_server(struct RemoteServer rs[])
+{
 	string q_str = getenv("QUERY_STRING");
 	stringstream ss1(q_str);
 	string str;
-	for(int i=0;i<MAXHOST;i++){
-		getline(ss1,str,'&');
-		rs[i].host = str.substr(3,str.size()-1);
-		getline(ss1,str,'&');
-		rs[i].port = str.substr(3,str.size()-1);
-		getline(ss1,str,'&');
-		rs[i].file = str.substr(3,str.size()-1);
+	for (int i = 0; i < MAXHOST; i++)
+	{
+		getline(ss1, str, '&');
+		rs[i].host = str.substr(3, str.size() - 1);
+		getline(ss1, str, '&');
+		rs[i].port = str.substr(3, str.size() - 1);
+		getline(ss1, str, '&');
+		rs[i].file = str.substr(3, str.size() - 1);
 		rs[i].id = i;
 	}
 }
 
-bool rs_selected(struct RemoteServer rs){
-	if(rs.host.size()&&rs.port.size()&&rs.file.size())
+bool rs_selected(struct RemoteServer rs)
+{
+	if (rs.host.size() && rs.port.size() && rs.file.size())
 		return true;
 	return false;
 }
 
 void first_page(struct RemoteServer rs[])
 {
-    	string rsp = 	"Content-Type: text/html\n\n"
-                 	"<!DOCTYPE html>\n"
-                	"<html lang=\"en\">\n"
-                 	"  <head>\n"
-                   	"    <meta charset=\"UTF-8\" />\n"
-                  	"    <title>NP Project 3 Console</title>\n"
-                   	"    <link\n"
-                   	"      rel=\"stylesheet\"\n"
-                   	"      href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\"\n"
-                   	"      integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\"\n"
-                   	"      crossorigin=\"anonymous\"\n"
-                   	"    />\n"
-                   	"    <link\n"
-                   	"      href=\"https://fonts.googleapis.com/css?family=Source+Code+Pro\"\n"
-                   	"      rel=\"stylesheet\"\n"
-                   	"    />\n"
-                   	"    <link\n"
-                   	"      rel=\"icon\"\n"
-                  	"      type=\"image/png\"\n"
-                   	"      href=\"https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678068-terminal-512.png\"\n"
-                  	"    />\n"
-                  	"    <style>\n"
-                  	"      * {\n"
-                  	"        font-family: 'Source Code Pro', monospace;\n"
-                  	"        font-size: 1rem !important;\n"
-                   	"      }\n"
-                   	"      body {\n"
-                   	"        background-color: #212529;\n"
-                   	"      }\n"
-                 	"      pre {\n"
-                 	"        color: #cccccc;\n"
-                 	"      }\n"
-                 	"      b {\n"
-                 	"        color: #ffffff;\n"
-                 	"      }\n"
-                 	"    </style>\n"
-                 	"  </head>\n"
-                 	"  <body>\n"
-                 	"    <table class=\"table table-dark table-bordered\">\n"
-                	"      <thead>\n"
-                	"        <tr>\n";
+	string rsp = "Content-Type: text/html\n\n"
+				 "<!DOCTYPE html>\n"
+				 "<html lang=\"en\">\n"
+				 "  <head>\n"
+				 "    <meta charset=\"UTF-8\" />\n"
+				 "    <title>NP Project 3 Console</title>\n"
+				 "    <link\n"
+				 "      rel=\"stylesheet\"\n"
+				 "      href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\"\n"
+				 "      integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\"\n"
+				 "      crossorigin=\"anonymous\"\n"
+				 "    />\n"
+				 "    <link\n"
+				 "      href=\"https://fonts.googleapis.com/css?family=Source+Code+Pro\"\n"
+				 "      rel=\"stylesheet\"\n"
+				 "    />\n"
+				 "    <link\n"
+				 "      rel=\"icon\"\n"
+				 "      type=\"image/png\"\n"
+				 "      href=\"https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678068-terminal-512.png\"\n"
+				 "    />\n"
+				 "    <style>\n"
+				 "      * {\n"
+				 "        font-family: 'Source Code Pro', monospace;\n"
+				 "        font-size: 1rem !important;\n"
+				 "      }\n"
+				 "      body {\n"
+				 "        background-color: #212529;\n"
+				 "      }\n"
+				 "      pre {\n"
+				 "        color: #cccccc;\n"
+				 "      }\n"
+				 "      b {\n"
+				 "        color: #ffffff;\n"
+				 "      }\n"
+				 "    </style>\n"
+				 "  </head>\n"
+				 "  <body>\n"
+				 "    <table class=\"table table-dark table-bordered\">\n"
+				 "      <thead>\n"
+				 "        <tr>\n";
 
-	for(int i=0;i<MAXHOST;i++) {
-        	if (rs_selected(rs[i])){
-            		rsp += "          <th scope=\"col\">" + rs[i].host + ":" + rs[i].port + "</th>\n";
-    		}
+	for (int i = 0; i < MAXHOST; i++)
+	{
+		if (rs_selected(rs[i]))
+		{
+			rsp += "          <th scope=\"col\">" + rs[i].host + ":" + rs[i].port + "</th>\n";
+		}
 	}
 
-    	rsp +=     "        </tr>\n"
-                   "      </thead>\n"
-                   "      <tbody>\n"
-                   "        <tr>\n";
-   	
-	for(int i=0;i<MAXHOST;i++) {
-       		 if (rs_selected(rs[i])){
-            		rsp += "          <td><pre id=\"s" + to_string(i) + "\" class=\"mb-0\"></pre></td>\n";
-		 }
+	rsp += "        </tr>\n"
+		   "      </thead>\n"
+		   "      <tbody>\n"
+		   "        <tr>\n";
+
+	for (int i = 0; i < MAXHOST; i++)
+	{
+		if (rs_selected(rs[i]))
+		{
+			rsp += "          <td><pre id=\"s" + to_string(i) + "\" class=\"mb-0\"></pre></td>\n";
+		}
 	}
-    	
-	rsp +=     "        </tr>\n"
-                   "      </tbody>\n"
-                   "    </table>\n"
-                   "  </body>\n"
-                   "</html>";
-	
-	write(1,rsp.c_str(),rsp.size()); 
+
+	rsp += "        </tr>\n"
+		   "      </tbody>\n"
+		   "    </table>\n"
+		   "  </body>\n"
+		   "</html>";
+
+	write(1, rsp.c_str(), rsp.size());
 }
 
+int main(int argc, char *const argv[])
+{
 
-int main(int argc, char* const argv[]) {
-  
-  try{
-  	struct RemoteServer RS[MAXHOST];
-	set_remote_server(RS);
-	first_page(RS);
- 	for(int i=0;i<MAXHOST;i++){
-  		if(rs_selected(RS[i])){
-			make_shared<ConsoleSession>(RS[i])->start();
+	try
+	{
+		struct RemoteServer RS[MAXHOST];
+		set_remote_server(RS);
+		first_page(RS);
+		for (int i = 0; i < MAXHOST; i++)
+		{
+			if (rs_selected(RS[i]))
+			{
+				make_shared<ConsoleSession>(RS[i])->start();
+			}
 		}
- 	}
-  	global_io_service.run();
-  }catch(exception& e){
-  	cerr << "Exception: " << e.what() << "\n"; 
-  }
-  return 0;
-
+		global_io_service.run();
+	}
+	catch (exception &e)
+	{
+		cerr << "Exception: " << e.what() << "\n";
+	}
+	return 0;
 }
--- http_server.cpp
+++ http_server.cpp
@@ -10,7 +10,8 @@
 using namespace std;
 using namespace boost::asio;
 
-struct ENV{
+struct ENV
+{
 	string req_method;
 	string req_uri;
 	string q_str;
@@ -24,131 +25,154 @@ struct ENV{
 
 io_service global_io_service;
 
-class HttpSession : public enable_shared_from_this<HttpSession> {
- private:
-  enum { max_length = 1024 };
-  ip::tcp::socket _socket;
-  array<char, max_length> _data;
+class HttpSession : public enable_shared_from_this<HttpSession>
+{
+private:
+	enum
+	{
+		max_length = 1024
+	};
+	ip::tcp::socket _socket;
+	array<char, max_length> _data;
 
- public:
-  HttpSession(ip::tcp::socket socket) : _socket(move(socket)) {}
+public:
+	HttpSession(ip::tcp::socket socket) : _socket(move(socket)) {}
 
-  void start() { do_read(); }
+	void start() { do_read(); }
 
- private:
-  void do_read() {
-    auto self(shared_from_this());
-    _socket.async_read_some(
-        buffer(_data, max_length),
-        [this, self](boost::system::error_code ec, size_t length) {
-		if(!ec){
-			string req(_data.data(),length);
-			
-			struct ENV env;
-			parse_request(req,env);
-			env.server_addr = _socket.local_endpoint().address().to_string();
-			env.server_port = to_string(_socket.local_endpoint().port());
-			env.remote_addr = _socket.remote_endpoint().address().to_string();
-			env.remote_port = to_string(_socket.remote_endpoint().port());			
+private:
+	void do_read()
+	{
+		auto self(shared_from_this());
+		_socket.async_read_some(
+			buffer(_data, max_length),
+			[this, self](boost::system::error_code ec, size_t length) {
+				if (!ec)
+				{
+					string req(_data.data(), length);
 
-			global_io_service.notify_fork(io_service::fork_prepare);
-			pid_t pid = fork();
-			if(pid == 0){	
-				global_io_service.notify_fork(io_service::fork_child);
-				set_env(env);
-				
-				dup2(_socket.native_handle(),STDIN_FILENO);
-				dup2(_socket.native_handle(),STDOUT_FILENO);
-				dup2(_socket.native_handle(),STDERR_FILENO);
-				_socket.close();	
+					struct ENV env;
+					parse_request(req, env);
+					env.server_addr = _socket.local_endpoint().address().to_string();
+					env.server_port = to_string(_socket.local_endpoint().port());
+					env.remote_addr = _socket.remote_endpoint().address().to_string();
+					env.remote_port = to_string(_socket.remote_endpoint().port());
 
-				string ok = env.server_protocol + "200 OK\n";
-				write(1,ok.c_str(),ok.size());	
+					global_io_service.notify_fork(io_service::fork_prepare);
+					pid_t pid = fork();
+					if (pid == 0)
+					{
+						global_io_service.notify_fork(io_service::fork_child);
+						set_env(env);
 
-				if(execl(env.req_uri.c_str(),env.req_uri.c_str(),NULL)<0){
-					string err = env.server_protocol + "500 Internal Server Error\n" ;
-					write(1,err.c_str(),err.size()); 
+						dup2(_socket.native_handle(), STDIN_FILENO);
+						dup2(_socket.native_handle(), STDOUT_FILENO);
+						dup2(_socket.native_handle(), STDERR_FILENO);
+						_socket.close();
+
+						string ok = env.server_protocol + "200 OK\n";
+						write(1, ok.c_str(), ok.size());
+
+						if (execl(env.req_uri.c_str(), env.req_uri.c_str(), NULL) < 0)
+						{
+							string err = env.server_protocol + "500 Internal Server Error\n";
+							write(1, err.c_str(), err.size());
+						}
+					}
+					else if (pid > 0)
+					{
+						global_io_service.notify_fork(io_service::fork_parent);
+						_socket.close();
+					}
 				}
+			});
+	}
 
-			}else if(pid > 0){
-				global_io_service.notify_fork(io_service::fork_parent);
-				_socket.close();	
+	void parse_request(string request, struct ENV &env)
+	{
+		stringstream ss1(request);
+		string str;
+		for (int line = 0; line < 2 && getline(ss1, str); line++)
+		{
+			stringstream ss2(str);
+			getline(ss2, str, '\r');
+			stringstream ss3(str);
+			if (line == 0)
+			{
+				ss3 >> env.req_method;
+				ss3 >> str;
+				ss3 >> env.server_protocol;
+				stringstream ss4(str);
+				getline(ss4, str, '?');
+				env.req_uri = str.substr(1, str.size() - 1);
+				getline(ss4, env.q_str, '?');
 			}
-		}	
-        });
-  }
-
-  void parse_request(string request,struct ENV& env){
-	stringstream ss1(request);
-	string str;
-	for(int line=0 ; line<2&&getline(ss1,str) ; line++){
-		stringstream ss2(str);
-		getline(ss2,str,'\r');
-		stringstream ss3(str);
-		if(line==0){
-			ss3 >> env.req_method;
-			ss3 >> str;
-			ss3 >> env.server_protocol;	
-			stringstream ss4(str);
-			getline(ss4,str,'?');
-			env.req_uri = str.substr(1,str.size()-1);
-			getline(ss4,env.q_str,'?');		
-		}else if(line==1){
-			ss3 >> str;
-			ss3 >> env.http_host;
+			else if (line == 1)
+			{
+				ss3 >> str;
+				ss3 >> env.http_host;
+			}
 		}
-	}	
-  }
+	}
 
-  void set_env(struct ENV& env){
-  	clearenv();
-	setenv("REQUEST_METHOD",env.req_method.c_str(),1);
-	setenv("REQUEST_URI",env.req_uri.c_str(),1);
-	setenv("QUERY_STRING",env.q_str.c_str(),1);
-	setenv("SERVER_PROTOCOL",env.server_protocol.c_str(),1);
-	setenv("HTTP_HOST",env.http_host.c_str(),1);
-	setenv("SERVER_ADDR",env.server_addr.c_str(),1);
-	setenv("SEVRER_PORT",env.server_port.c_str(),1);
-	setenv("REMOTE_ADDR",env.remote_addr.c_str(),1);
-	setenv("REMOTE_PORT",env.remote_port.c_str(),1);
-  }
-
+	void set_env(struct ENV &env)
+	{
+		clearenv();
+		setenv("REQUEST_METHOD", env.req_method.c_str(), 1);
+		setenv("REQUEST_URI", env.req_uri.c_str(), 1);
+		setenv("QUERY_STRING", env.q_str.c_str(), 1);
+		setenv("SERVER_PROTOCOL", env.server_protocol.c_str(), 1);
+		setenv("HTTP_HOST", env.http_host.c_str(), 1);
+		setenv("SERVER_ADDR", env.server_addr.c_str(), 1);
+		setenv("SEVRER_PORT", env.server_port.c_str(), 1);
+		setenv("REMOTE_ADDR", env.remote_addr.c_str(), 1);
+		setenv("REMOTE_PORT", env.remote_port.c_str(), 1);
+	}
 };
 
-class HttpServer {
- private:
-  ip::tcp::acceptor _acceptor;
-  ip::tcp::socket _socket;
+class HttpServer
+{
+private:
+	ip::tcp::acceptor _acceptor;
+	ip::tcp::socket _socket;
 
- public:
-  HttpServer(short port)
-      : _acceptor(global_io_service, ip::tcp::endpoint(ip::tcp::v4(), port)),
-        _socket(global_io_service) {
-    do_accept();
-  }
+public:
+	HttpServer(short port)
+		: _acceptor(global_io_service, ip::tcp::endpoint(ip::tcp::v4(), port)),
+		  _socket(global_io_service)
+	{
+		do_accept();
+	}
 
- private:
-  void do_accept() {
-    _acceptor.async_accept(_socket, [this](boost::system::error_code ec) {
-	if (!ec) make_shared<HttpSession>(move(_socket))->start();	
-      	do_accept();
-    });
-  }
+private:
+	void do_accept()
+	{
+		_acceptor.async_accept(_socket, [this](boost::system::error_code ec) {
+			if (!ec)
+				make_shared<HttpSession>(move(_socket))->start();
+			do_accept();
+		});
+	}
 };
 
-int main(int argc, char* const argv[]) {
-  if (argc != 2) {
-    cerr << "Usage:" << argv[0] << " [port]" << endl;
-    return 1;
-  }
+int main(int argc, char *const argv[])
+{
+	if (argc != 2)
+	{
+		cerr << "Usage:" << argv[0] << " [port]" << endl;
+		return 1;
+	}
 
-  try {
-    unsigned short port = atoi(argv[1]);
-    HttpServer server(port);
-    global_io_service.run();
-  } catch (exception& e) {
-    cerr << "Exception: " << e.what() << "\n";
-  }
+	try
+	{
+		unsigned short port = atoi(argv[1]);
+		HttpServer server(port);
+		global_io_service.run();
+	}
+	catch (exception &e)
+	{
+		cerr << "Exception: " << e.what() << "\n";
+	}
 
-  return 0;
+	return 0;
 }
